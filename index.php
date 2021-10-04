b411d186d0ba7b3c6c3620bb1d5297d70fbcf6af
Syamil Shamsuddin <syamilstar123@gmail.com>
2021-10-04 12:43:36 +0800

    Create index.php
    Latihan 03

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 03</title>
+</head>
+<body>
+    <h1>ASAS PHP</h1>
+
+    <?php
+        $namapelajar = 'Ali';
+         echo '<h2>Hello</h2>'; #output
+        $hadir = false;
+        $umur= 20;
+        
+        echo 20.21;
+
+        $x = 100; #integer
+        $y = '100'; #string
+
+        if ($x == $y){
+            echo 'Sama';
+        }
+        else{
+            echo 'Berbeza';
+        }
+    
+    
+    ?>
+</body>
+</html>
